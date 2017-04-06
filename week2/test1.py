a44acb421dbdf5793aad2fdfa385962b5a4c41f5
sgreen1 <sgreen1@gmail.com>
2017-04-06 15:14:39 -0700

    thangs

--- week2/test1.py
+++ week2/test1.py
@@ -9,19 +9,23 @@ import time
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
 
+def login(remote_conn, username, password):
+    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    remote_conn.write(username + '\n')
+    output += remote_conn.read_until
+    remote_conn.write(password + '\n')
+    return output
+    
+
 def main():
     ip_addr = '184.105.247.70'
     username = 'pyclass' 
     password = '88newclass'
 
     remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
-    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    output = login(remote_conn, username, password)
     
-    remote_conn.write(username + '\n')
-    output = remote_conn.read_until("assword:", TELNET_TIMEOUT)
-
-    remote_conn.write(password + '\n') 
-    time.sleep(1)
+    time.sleep(0)
     output = remote_conn.read_very_eager()
 
     remote_conn.write("terminal length 0" + '\n')
