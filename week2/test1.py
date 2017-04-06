468a73554675b113fba65aa0a37ab578c9e899c0
sgreen1 <sgreen1@gmail.com>
2017-04-06 13:45:44 -0700

    stuffnthangs

--- week2/test1.py
+++ week2/test1.py
@@ -4,8 +4,40 @@ import telnetlib
 import re
 from pprint import pprint as pp
 from ciscoconfparse import CiscoConfParse
+import time 
+
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def main():
+    ip_addr = '184.105.247.70'
+    username = 'pyclass' 
+    password = '88newclass'
+
+    remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    
+    remote_conn.write(username + '\n')
+    output = remote_conn.read_until("assword:", TELNET_TIMEOUT)
+
+    remote_conn.write(password + '\n') 
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
+
+    remote_conn.write("terminal length 0" + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
+
+    remote_conn.write("show version" + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
+    print(output)
+
+
+#    remote_conn.close() 
 
 
 
 if __name__ == "__main__":
     main()
+
